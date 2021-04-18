a52ca73f79b3d5fa0b9a671c9b65af58790ec919
TedOss <tobello@yahoo.com>
2021-04-18 18:16:18 -0400

    CALC-200:Add unit Tests for Integer Multiply Model.

---
+++ src/test/java/com/belloot/tech/AppMultiplyIntegers_Test.java
@@ -0,0 +1,20 @@
+package com.belloot.tech;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import Model.Multiply;
+
+public class AppMultiplyIntegers_Test {
+
+    /**
+     * Unit test for simple App.
+     */
+        Multiply multip = new Multiply();
+
+        @Test
+        public void Multiplying_2_Numbers_ShouldBeAccurate(){
+            Integer nb1 = 10, nb2 = 10;
+            assertEquals( (long)multip.perform(10, 10), 100);
+        }
+}
